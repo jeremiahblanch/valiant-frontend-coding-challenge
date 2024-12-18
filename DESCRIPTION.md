440c405f561c3512fa5723354b6ac67c25a7bb0f
Jeremiah Blanch <jeremiahblanch@gmail.com>
2024-12-18 13:57:41 +1100

    :tada: Initial commit: basic input and calculating PMT using hard-coded values

--- DESCRIPTION.md
+++ DESCRIPTION.md
@@ -1,1 +1,8 @@
-TODO: Quick scratch breakdown of your to-dos, how you broke up the different tasks and any QA / tests you did.
+
+TODOs:
+1. Make simple loan amount input that when changed recalculates the PMT using constants for the other values.
+2. Fetch the values from the apis and display them on screen
+3. Make select components
+4. Use values from select components to change params for PMT calculations.
+2. Make a component for the loan amount input
+ 
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,79 @@
 <script setup>
-defineOptions({
-  name: 'App',
+import { ref, watch } from 'vue'
+import PMT from './utils/PMT'
+
+const error = ref('')
+const isValid = ref(false)
+const loanAmount = ref(0)
+const repaymentAmountPerPeriod = ref(0)
+const totalRepayments = ref(0)
+
+function recalc (principal) {
+  const countPeriods = 24 // TODO
+  const periodsPerYear = 12 // TODO
+  const ratePerTerm = 0.07 / periodsPerYear // TODO
+
+  const result = PMT(
+    ratePerTerm,
+    countPeriods,
+    principal
+  )
+
+  isValid.value = !isNaN(result)
+  if (isValid.value) {
+    const repayment = result * -1
+
+    repaymentAmountPerPeriod.value = Math.ceil(repayment)
+    totalRepayments.value = Math.ceil(repayment * countPeriods)
+  } else {
+    error.value = 'Could not calculate'
+  }
+}
+
+function clear () {
+  error.value = ''
+  isValid.value = false
+  repaymentAmountPerPeriod.value = 0
+  totalRepayments.value = 0
+}
+
+watch(loanAmount, (newValue) => {
+  clear()
+  if (isNaN(Number(newValue))) {
+    error.value = 'Please enter a number'
+    isValid.value = false
+  }
+  if (newValue > 1000) {
+    recalc(newValue)
+  }
 })
 </script>
 
 <template>
-  <!-- TODO: Complete the coding challenge here! -->
-  <div />
+  <div>
+    <label
+      for="loanAmountInput"
+    >
+      Loan Amount
+    </label>
+    <input
+      id="loanAmountInput"
+      v-model="loanAmount"
+      type="text"
+    >
+  </div>
+
+  <div
+    v-if="error"
+    class="text-red-500"
+  >
+    Error: {{ error }}
+  </div>
+
+  <div
+    v-else-if="isValid"
+  >
+    <p>{{ repaymentAmountPerPeriod }} monthly repayments</p>
+    <p>{{ totalRepayments }} total repayments</p>
+  </div>
 </template>

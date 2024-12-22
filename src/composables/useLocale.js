8b3ec22b72e72e23cecf1a901096f18c6a0c9a46
Jeremiah Blanch <jeremiahblanch@gmail.com>
2024-12-23 09:53:09 +1100

    :bulb: Fix typo

--- src/composables/useLocale.js
+++ src/composables/useLocale.js
@@ -1,5 +1,5 @@
 export function useLocale () {
-  // this is the best way to get the current lcoale for the browser see https://stackoverflow.com/a/57529410
+  // this is the best way to get the current locale for the browser see https://stackoverflow.com/a/57529410
   const locale = Intl.NumberFormat().resolvedOptions().locale
 
   return { locale }

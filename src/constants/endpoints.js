8f06980290a1d97189e3e3541e994197acdf552c
Jeremiah Blanch <jeremiahblanch@gmail.com>
2025-01-07 10:00:05 +1100

    :bulb: add TODO about API origin

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,4 +1,4 @@
-const ENDPOINT_ORIGIN = 'http://localhost:5000'
+const ENDPOINT_ORIGIN = 'http://localhost:5000' // TODO - should be set via a process env variable
 
 export const ENDPOINT_LOAN_PURPOSES = `${ENDPOINT_ORIGIN}/loan-purposes`
 export const ENDPOINT_REQUESTED_REPAYMENT_PERIODS = `${ENDPOINT_ORIGIN}/requested-repayment-periods`
